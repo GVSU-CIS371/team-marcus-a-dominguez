ae666585bce69bd402fe4efcd242bd82ec506f36
mabaddomm <cus.dominguez@gmail.com>
2026-04-24 18:31:10 +0000

    The projects stuff

---
+++ legoApp/src/main.ts
@@ -0,0 +1,27 @@
+import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import { createRouter, createWebHashHistory} from "vue-router"
+
+import './style.css'
+import App from './App.vue'
+import Home from './views/homeView.vue'
+import test from './views/testView.vue'
+
+
+const routes = [
+    {path: "/", component: Home},
+    {path: "/test", component: test }
+]
+
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({components, directives, theme: { defaultTheme: 'light'}})
+createApp(App).use(vuetify).use(router).mount('#app')
---
+++ legoApp/src/views/homeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Header from '../components/HeaderTest.vue'
+</script>
+
+<template>
+  <div class="HomeHeader">
+    <Header/>
+  </div>
+</template>
