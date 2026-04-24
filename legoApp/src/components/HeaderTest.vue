70e387cb4a0c549885b5732ec0548974727a5c98
mabaddomm <cus.dominguez@gmail.com>
2026-04-24 18:30:18 +0000

    adding basics stuffs

---
+++ legoApp/src/components/HeaderTest.vue
@@ -0,0 +1,24 @@
+ <script setup lang="ts">
+    import { ref } from 'vue'
+
+    const showRightDrawer = ref(false)
+
+</script>
+
+<template>
+    <v-app>
+        <v-app-bar class="legoHeader">
+            <v-app-bar-title>Lego Collectors</v-app-bar-title>
+            <v-app-bar-nav-icon @click="showRightDrawer = !showRightDrawer"></v-app-bar-nav-icon>
+        </v-app-bar>
+        <v-navigation-drawer location="right" permanent v-model="showRightDrawer">
+        </v-navigation-drawer>
+        <v-main>
+            <ul>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+            </ul>
+        </v-main>
+    </v-app>
+</template>
